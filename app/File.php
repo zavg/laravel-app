2c386dc51245d8dca8a54b00a7f15d1ee51f1c19
zavg <sergey.zavg@gmail.com>
2020-11-06 10:54:28 +0300

    File model (github repo file abstraction)

---
+++ app/File.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'path', 'lines', 'repo_id'
+    ];
+}
